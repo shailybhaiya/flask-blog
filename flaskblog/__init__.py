becc4f285182a35d39155f614c9ec62909916c02
unknown <bhaiya18shaily@gmail.com>
2020-07-11 22:14:49 +0530

    first commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+app = Flask(__name__)
+app.config['SECRET_KEY']='769cd8392c228e7264ff55ec32d7ae93'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message_category='info'
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USERNAME']=os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD']=os.environ.get('EMAIL_PASSWORD')
+mail=Mail(app)
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
